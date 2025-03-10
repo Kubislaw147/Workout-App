1b3f16e39dd6c4bbec0d142cf2d8b04afc97926f
Jakub Jedynak <kubajed07@gmail.com>
2025-03-10 18:57:45 +0100

    shadcn / SetFieldForm added

--- src/assets/components/ExerciseField.tsx
+++ src/assets/components/ExerciseField.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import SetField from "./SetField";
 
 interface props {
--- src/assets/components/SetField.tsx
+++ src/assets/components/SetField.tsx
@@ -1,17 +1,73 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  weight: z.number,
+  reps: z.number,
+});
 
 const SetField = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      weight: "",
+      reps: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
     <>
-      <div className="border-2 border-red-600 p-1 rounded-lg m-1">
+      <div className="border-2 border-red-900 p-1 rounded-lg m-1">
         <div className="inline">reps </div>
         <div className="inline">rest time</div>
-        <form className="inline">
-          <input type="number" name="weight" className="w-15 "></input>
-          <label>KG x </label>
-          <input type="number" name="set" className="w-15"></input>
-          <button type="submit">Zatwierdz</button>
-        </form>
+        <div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="weight"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="weight" {...field} />
+                    </FormControl>
+                    <FormLabel>KG</FormLabel>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="reps"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel> x </FormLabel>
+                    <FormControl>
+                      <Input placeholder="reps" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </div>
       </div>
     </>
   );
